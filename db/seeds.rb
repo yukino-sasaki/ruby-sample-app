d2087bdce8c6d278547623c46005ac8902b533c3
yukino-sasaki <yukisnowman.18@gmail.com>
2023-06-18 20:38:44 +0900

    chore: save tempolary

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar", admin: true
+             password_confirmation: "foobar", admin: true,
              activated: true,
              activated_at: Time.zone.now)
 # 追加のユーザーをまとめて生成する
@@ -20,7 +20,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password
+               password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)              
 end
